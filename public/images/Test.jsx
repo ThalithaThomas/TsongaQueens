d3ce57f3f27ff5f8edb13315dd1bffe6fb354ef9
ThalithaThomas <105857301+ThalithaThomas@users.noreply.github.com>
2025-03-21 22:42:31 +0200

    update path for images in productArr

---
+++ public/images/Test.jsx
@@ -0,0 +1,19 @@
+const Test = () => {
+  return (
+    <div className="flex flex-col items-center bg-gray-100 ">
+      <h1 className=" pb-1 font-bold hidden md:block md:font-semibold md:text-2xl ">
+        STAND OUT
+      </h1>
+      <h1 className="font-Playfair mt-4 font-thin text-4xl md:hidden">
+        Xitsonga Couture
+      </h1>
+      <p className="hidden md:block font-extralight">
+        Make a statement with Tsonga traditional wear attire! choose <br /> one
+        of our elegant pieces to elevate your wardrobe today.
+      </p>
+      <img src="/frontend/public" alt="" />
+    </div>
+  );
+};
+
+export default Test;
